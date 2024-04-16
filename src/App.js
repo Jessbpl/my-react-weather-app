79cbdffab27794814de3c1ed524a373244942a24
Jessbpl <jessbrapl@gmail.com>
2024-04-16 22:32:55 +0200

    Added Github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Jessica Bravo and
+        <a
+          href="https://github.com/Jessbpl/my-react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          is open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
