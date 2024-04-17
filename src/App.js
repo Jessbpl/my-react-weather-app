f72b82aaa81ee1ff1968d785888dcb6852e8cf0d
Jessbpl <jessbrapl@gmail.com>
2024-04-17 20:21:19 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Jessica Bravo and
-        <a
-          href="https://github.com/Jessbpl/my-react-weather-app"
-          target="_blank"
-        >
-          {" "}
-          is open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Jessica Bravo and
+          <a
+            href="https://github.com/Jessbpl/my-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            is open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
